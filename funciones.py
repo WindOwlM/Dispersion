4e66ded8179523b2d783d85d2732368665a970b1
Maicol <mamian642@gmail.com>
2025-06-09 11:48:01 -0500

    V 0.3
    Se arregla problema con la linea final

--- funciones.py
+++ funciones.py
@@ -4,12 +4,12 @@ def crear_dispersion(tipo_documento,documento,primer_apellido,segundo_apellido,p
     
     documento = str(int(documento)).zfill(15)
     
-    linea_completa = f"{tipo_documento}{documento}"
-    
     primer_apellido = str(primer_apellido).ljust(15,' ')
     segundo_apellido = str(segundo_apellido).ljust(15,' ')
 
     primer_nombre = str(primer_nombre).ljust(15,' ')
     segundo_nombre = str(segundo_nombre).ljust(15,' ')
+    
+    linea_completa = f"{tipo_documento}{documento}{primer_apellido}{segundo_apellido}{primer_nombre}{segundo_nombre}"
     
     return linea_completa
